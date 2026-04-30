453b173b6a98c9873506b696ace98b5c62fbe76f
Artur Vale <artursantosvale@gmail.com>
2026-04-30 16:33:35 -0300

    Inclusão de manifesto para ignorar esticamento de DPI em monitores com zoom

--- kpatcher/build.rs
+++ kpatcher/build.rs
@@ -3,6 +3,21 @@ fn main() {
     let mut res = winres::WindowsResource::new();
     // The icon path is relative to the Cargo.toml directory
     res.set_icon("resources/kpatcher.ico");
+    // Embed DPI-awareness manifest (Per-Monitor V2) to prevent
+    // Windows bitmap scaling on notebooks with display scaling (110%, 125%, etc.)
+    res.set_manifest(r#"<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+  <application xmlns="urn:schemas-microsoft-com:asm.v3">
+    <windowsSettings>
+      <dpiAwareness xmlns="http://schemas.microsoft.com/SMI/2016/WindowsSettings">
+        PerMonitorV2, PerMonitor
+      </dpiAwareness>
+      <dpiAware xmlns="http://schemas.microsoft.com/SMI/2005/WindowsSettings">
+        True/PM
+      </dpiAware>
+    </windowsSettings>
+  </application>
+</assembly>"#);
 
     // Note: winres will automatically use the [package.metadata.winres] info from Cargo.toml
     // for version info, etc.
